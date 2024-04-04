c38124f09596a58fd34a5612cadbfe07ce3a2ec4
knqiufan <knqiufan@foxmail.com>
2024-04-04 11:33:11 +0800

    update provider pom

---
+++ knqiufan-rpc-demo-provider/src/test/java/cn/knqiufan/rpc/demo/provider/KnrpcDemoProviderApplicationTest.java
@@ -0,0 +1,20 @@
+package cn.knqiufan.rpc.demo.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 单元测试
+ *
+ * @author knqiufan
+ * @version 1.0.0
+ * @date 2024/4/4 10:59
+ */
+@SpringBootTest
+public class KnrpcDemoProviderApplicationTest {
+
+  @Test
+  public void test() {
+
+  }
+}
